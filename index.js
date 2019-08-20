0d5f515b79fad06bb0736c4fb63fa2631c9b7320
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-08-20 10:06:32 -0300

    + Created script to execute nodemon. + Created insert, edit and read projects CRUD.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const express = require("express");
 
 const server = express();
+
+const projects = [];
+
+server.use(express.json());
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({ id, title });
+
+  return res.json(projects);
+});
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { title } = req.body;
+  const { id } = req.params;
+
+  const project = projects.find(element => element.id === id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.listen(3000);
